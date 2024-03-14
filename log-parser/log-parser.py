b6e6aeffcfe9265bddce80ee3448a08d533d9cea
Niflnir <shaunchenggy@gmail.com>
2024-03-14 23:13:52 +0800

    feat: add request duration metric

--- log-parser/log-parser.py
+++ log-parser/log-parser.py
@@ -1,6 +1,6 @@
 import time
 import re
-from prometheus_client import start_http_server, disable_created_metrics, Counter, Gauge
+from prometheus_client import start_http_server, disable_created_metrics, Counter, Gauge, Histogram
 
 log_file_path = '/opt/decoding-sdk-logs/master_server.log'
 SERVER_REQUESTS = Counter('server_requests', 'Number of requests received by the server')
@@ -8,12 +8,36 @@ DECODING_REQUESTS_FAILED = Counter('decoding_requests_failed', 'Number of failed
 DECODING_REQUESTS_SUCCESSFUL = Counter('decoding_requests_successful', 'Number of successful decoding requests')
 DECODING_REQUESTS_TOTAL = Counter('decoding_requests_total', 'Total number of decoding requests', ['status'])
 SPEECH_WORKER_COUNT = Gauge('speech_worker_count', 'Number of speech workers currently available')
+REQUEST_DURATION = Histogram('request_duration_milliseconds', 'Time taken to complete a request', ['request_id'])
 
 
-def update_server_requests_metric(log_line):
-    # The server logs "OPEN" when it receives an incoming request from the client
-    if "OPEN" in log_line:
+def calculate_duration_milliseconds(start_time, end_time):
+    start_time_parts = start_time.split(',')
+    end_time_parts = end_time.split(',')
+    start_seconds = int(start_time_parts[0].replace(':', ''))
+    end_seconds = int(end_time_parts[0].replace(':', ''))
+    return (end_seconds - start_seconds) * 1000 + (int(end_time_parts[1]) - int(start_time_parts[1]))
+
+
+def update_request_count_and_duration_metrics(log_line):
+    global start_time
+    global end_time
+
+    request_start_pattern = re.compile(r'INFO.* (\d{2}:\d{2}:\d{2},\d{3}) (\w+-\w+-\w+-\w+-\w+): OPEN')
+    request_end_pattern = re.compile(r'INFO.* (\d{2}:\d{2}:\d{2},\d{3}) (\w+-\w+-\w+-\w+-\w+): Sending event')
+
+    request_start_match = request_start_pattern.search(log_line)
+    if request_start_match:
+        start_time, _ = request_start_match.groups()
         SERVER_REQUESTS.inc()
+        return
+
+    request_end_match = request_end_pattern.search(log_line)
+    if request_end_match:
+        end_time, end_request_id = request_end_match.groups()
+
+        duration_milliseconds = calculate_duration_milliseconds(start_time, end_time)
+        REQUEST_DURATION.labels(request_id=end_request_id).observe(duration_milliseconds)
 
 
 def update_decoding_requests_metrics(log_line):
@@ -42,9 +66,9 @@ def update_speech_worker_count_metric(log_line):
 
 
 def parse_log(log_line):
-    update_server_requests_metric(log_line)
     update_speech_worker_count_metric(log_line)
     update_decoding_requests_metrics(log_line)
+    update_request_count_and_duration_metrics(log_line)
 
 
 def main():
