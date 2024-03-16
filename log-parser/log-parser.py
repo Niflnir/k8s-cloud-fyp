a23d1693e62c4c671e2b785ddcc98744d2c73992
Niflnir <shaunchenggy@gmail.com>
2024-03-17 00:36:59 +0800

    feat: add request latency metric and use pod logs

--- log-parser/log-parser.py
+++ log-parser/log-parser.py
@@ -1,14 +1,13 @@
-import time
 import re
+from kubernetes import client, config, watch
 from prometheus_client import start_http_server, disable_created_metrics, Counter, Gauge, Histogram
 
-log_file_path = '/opt/decoding-sdk-logs/master_server.log'
 SERVER_REQUESTS = Counter('server_requests', 'Number of requests received by the server')
 DECODING_REQUESTS_FAILED = Counter('decoding_requests_failed', 'Number of failed decoding requests')
 DECODING_REQUESTS_SUCCESSFUL = Counter('decoding_requests_successful', 'Number of successful decoding requests')
-DECODING_REQUESTS_TOTAL = Counter('decoding_requests_total', 'Total number of decoding requests', ['status'])
 SPEECH_WORKER_COUNT = Gauge('speech_worker_count', 'Number of speech workers currently available')
 REQUEST_DURATION = Histogram('request_duration_milliseconds', 'Time taken to complete a request', ['request_id'])
+REQUEST_LATENCY = Histogram('request_latency_milliseconds', 'Request latency in milliseconds')
 
 
 def calculate_duration_milliseconds(start_time, end_time):
@@ -17,6 +16,14 @@ def calculate_duration_milliseconds(start_time, end_time):
     start_seconds = int(start_time_parts[0].replace(':', ''))
     end_seconds = int(end_time_parts[0].replace(':', ''))
     return (end_seconds - start_seconds) * 1000 + (int(end_time_parts[1]) - int(start_time_parts[1]))
+
+
+def update_latency_metric(log_line):
+    pattern = r".*101 GET.*?(\d+\.\d+)ms"
+    match = re.search(pattern, log_line)
+    if match:
+        latency_milliseconds = float(match.group(1))
+        REQUEST_LATENCY.observe(latency_milliseconds)
 
 
 def update_request_count_and_duration_metrics(log_line):
@@ -41,7 +48,7 @@ def update_request_count_and_duration_metrics(log_line):
 
 
 def update_decoding_requests_metrics(log_line):
-    pattern = r"Sending event \{'status': (\d+).*"
+    pattern = r"INFO.* Sending event \{'status': (\d+).*"
     match = re.search(pattern, log_line)
     if match:
         status = int(match.group(1))
@@ -55,11 +62,9 @@ def update_decoding_requests_metrics(log_line):
         else:
             DECODING_REQUESTS_FAILED.inc()
 
-        DECODING_REQUESTS_TOTAL.labels(status=status).inc()
-
 
 def update_speech_worker_count_metric(log_line):
-    pattern = r'Number of worker available (\d+)'
+    pattern = r'INFO.* Number of worker available (\d+)'
     match = re.search(pattern, log_line)
     if match:
         SPEECH_WORKER_COUNT.set(int(match.group(1)))
@@ -69,20 +74,28 @@ def parse_log(log_line):
     update_speech_worker_count_metric(log_line)
     update_decoding_requests_metrics(log_line)
     update_request_count_and_duration_metrics(log_line)
+    update_latency_metric(log_line)
 
 
 def main():
     disable_created_metrics()
     start_http_server(8080)
-    with open(log_file_path, 'r') as log_file:
-        while True:
-            line = log_file.readline()
-            if not line:
-                time.sleep(1)
-                continue
-            parse_log(line)
+
+    config.load_incluster_config()
+    v1 = client.CoreV1Api()
+
+    label_selector = 'app=decoding-sdk-server'
+    namespace = 'monitoring'
+    container = 'decoding-sdk-server'
+
+    ret = v1.list_namespaced_pod(namespace=namespace, label_selector=label_selector)
+
+    pod = ret.items[0]
+    pod_name = pod.metadata.name
+    w = watch.Watch()
+    for log_line in w.stream(v1.read_namespaced_pod_log, name=pod_name, namespace=namespace, container=container, tail_lines=1, follow=True):
+        parse_log(log_line)
 
 
 if __name__ == "__main__":
     main()
-
